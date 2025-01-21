ce04a7f4987bdbb7808e33168049b441fe14d07b
higunito <reisen0615akira@gmail.com>
2025-01-21 22:35:34 +0900

    add comments

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,3 +1,12 @@
+// 更新関数の型について
+// type SetStateAction<T> = T | ((prevState: T) => T);
+// Tは現在の状態の型(string))。右は現在の状態prevState: Tから新しい状態Tへ値を更新する関数型
+// type Dispatch<A> = (value: A) => void;
+// Dispatch<A>は、1つの引数 value を受け取り、それを使って処理を実行する関数型
+//つまり、setStateは次のような関数型
+// (value: string | ((prevState: string) => string)) => void;
+
+
 type TodoFormProps = {
   text: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
